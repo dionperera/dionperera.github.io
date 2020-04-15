e44e906bf4573f9e7844f23f0b2e795ab9d23061
dionperera <36972169+dionperera@users.noreply.github.com>
2020-04-14 20:38:44 -0400

    Files Added to the Repository
    Combined Weebly with github

---
+++ files/theme/files/dion/CMShack.bat
@@ -0,0 +1,9 @@
+@echo off
+title CMS is Hacking
+color 0a
+mode 1000
+start talk.vbs
+:a
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto a
+ start talk.vbs
---
+++ files/theme/files/script.js
@@ -0,0 +1,24 @@
+</SCRIPT>
+</head>
+<body>
+<SCRIPT language="JavaScript">
+<!--hide
+ 
+var password;
+ 
+var pass1="diondabestisthebest";
+ 
+password=prompt('Please enter your password to view the page!',' ');
+ 
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    window.location="https://diondabest.weebly.com/wrong-password.html";
+    }
+ 
+//-->
+</SCRIPT>
+
+</body>
+</html>
