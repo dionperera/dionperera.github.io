b90dc8c07583e48ecefc4ea8171323b70ca71d38
dionperera <36972169+dionperera@users.noreply.github.com>
2020-07-22 01:52:19 -0400

    Add files via upload

---
+++ forward.js
@@ -0,0 +1,3 @@
+var loc = window.location.href+''; if (loc.indexOf('http://dion.gov')-true){window.location.replace("http://dion.gov/test.html") }
+var loc = window.location.href+''; if (loc.indexOf('http://dion.gq')-true){window.location.replace("https://bit.ly") }
+var loc = window.location.href+''; if (loc.indexOf('http://link.dionperera.com')-true){window.location.replace("https://bit.ly") }
