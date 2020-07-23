1fb2db6d5f986317ebefed2e83c3a16be8a4e923
dionperera <36972169+dionperera@users.noreply.github.com>
2020-07-22 22:23:27 -0400

    Update forward.js

--- forward.js
+++ forward.js
@@ -1,3 +1,2 @@
-var loc = window.location.href+''; if (loc.indexOf('http://dion.gov')==true){window.location.replace("http://dion.gov/test.html") }
-var loc = window.location.href+''; if (loc.indexOf('http://dion.gq')==true){window.location.replace("https://bit.ly") }
-var loc = window.location.href+''; if (loc.indexOf('http://link.dionperera.com')==true){window.location.replace("https://bit.ly") }
+if (window.location.hostname == "dion.gov") {window.location.replace("http://dion.gov/test.html");
+}
