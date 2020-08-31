f503111cee16c6c26033ca2178f2be5d905e6461
dionperera <36972169+dionperera@users.noreply.github.com>
2020-08-31 14:42:18 -0400

    Update forward.js

--- forward.js
+++ forward.js
@@ -1,2 +1,5 @@
 if (window.location.hostname == "dion.gov") {window.location.replace("http://dion.gov/test.html");}
 if (window.location.hostname == "filter.meetcircle.com"){window.location.replace("https://www.dionperera.com");}
+if (window.location.hostname == "dionperera.com"){console.log('Hello From the Console!');}
+if (window.location.hostname == "www.dionperera.com"){console.log('Hello From the Console!');}
+else {window.location.replace("https://www.dionperera.com") }
