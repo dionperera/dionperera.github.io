7ba9ce5d84a4d1d249b88912de33f47dcb6da252
dionperera <36972169+dionperera@users.noreply.github.com>
2020-08-31 13:53:09 -0400

    Update forward.js

--- forward.js
+++ forward.js
@@ -1,2 +1,4 @@
 if (window.location.hostname == "dion.gov") {window.location.replace("http://dion.gov/test.html");
 }
+else if <script language="JavaScript"> var loc = window.location.href+''; if (loc.indexOf('https://www.dionperera.com')-false){window.location.replace("https://www.dionperera.com")
+}
