1fbd5ff072189572a35c847e0472965742f94339
dionperera <36972169+dionperera@users.noreply.github.com>
2020-08-31 14:33:13 -0400

    Update forward.js

--- forward.js
+++ forward.js
@@ -1,4 +1,2 @@
-if (window.location.hostname == "dion.gov") {window.location.replace("http://dion.gov/test.html");
-}
-else if <script language="JavaScript"> var loc = window.location.href+''; if (loc.indexOf('https://www.dionperera.com')-false){window.location.replace("https://www.dionperera.com")
-}
+if (window.location.hostname == "dion.gov") {window.location.replace("http://dion.gov/test.html");}
+if (window.location.hostname == "filter.meetcircle.com"){window.location.replace("https://www.dionperera.com");}
