ec4fc4defb19af134dec6d959c10b411a3cfe2e5
dionperera <36972169+dionperera@users.noreply.github.com>
2020-09-09 10:31:21 -0400

    Update forward.js

--- forward.js
+++ forward.js
@@ -3,4 +3,3 @@ if (window.location.hostname == "filter.meetcircle.com"){window.location.replace
 if (window.location.hostname == "dionperera.com"){console.log('Hello From the Console!');}
 if (window.location.hostname == "www.dionperera.com"){console.log('Hello From the Console!');}
 if (window.location.hostname == "test1.dionperera.com"){console.log('Hello From the Console reeee!');}
-else {window.location.replace("https://www.dionperera.com") }
