420d7c9dd7da9508f060471ba6093c7f0064d4a2
dionperera <36972169+dionperera@users.noreply.github.com>
2020-10-20 12:11:27 -0400

    Update forward.js

--- forward.js
+++ forward.js
@@ -5,4 +5,6 @@ else if (window.location.hostname == "dionperera.com"){console.log('WS-ML');}
 else if (window.location.hostname == "www.dionperera.com"){console.log('WM');}
 else if (window.location.hostname == "test.dionperera.com"){console.log('T0');}
 else if (window.location.hostname == "b.dionperera.com"){console.log('CDN-CFD');}
+else if (window.location.hostname == "ipv6.dionperera.com"){console.log('IPv6');}
+else if (window.location.hostname == "ipv4.dionperera.com"){console.log('IPv4');}
 else {window.location.replace("https://www.dionperera.com");}
